4a1be4ecadcba98e92b3ad044bd77aceee9d2dd9
Max Base <Max@Asrez.com>
2020-02-10 00:07:02 +0330

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,1 +1,45 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import networkx as nx
+import random
 
+
+def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
+    if not nx.is_tree(G):
+        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
+        if pos is None:
+            pos = {root:(xcenter,vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)
+        if len(children)!=0:
+            dx = width/len(children)
+            nextx = xcenter - width/2 - dx/2
+            for child in children:
+                nextx += dx
+                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap,
+                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
+                                    pos=pos, parent = root)
+        return pos
+
+
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
+G=nx.Graph()
+G.add_edges_from([(1,2), (1,3), (1,4), (2,5), (2,6), (2,7), (3,8), (3,9), (4,10),
+                  (5,11), (5,12), (6,13)])
+"""G.add_edges_from(
+    [('Y ','X'),('Y','1 '),('Y','X'),('Y ','Z'),('Y ','Z '),('Z',' 1'),('Z ','1')
+     ])"""
+pos = hierarchy_pos(G,1)
+nx.draw(G, pos=pos, with_labels=True)
+plt.show()
