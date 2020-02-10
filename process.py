575d4721d4ccf5c3837a9388c10adb3eb74ba77c
Max Base <Max@Asrez.com>
2020-02-10 23:08:03 +0330

    Update process.py

--- process.py
+++ process.py
@@ -2,22 +2,37 @@
 # https://github.com/BaseMax/PhdMathProject
 import string
 data=[
-	[0,0,1],
+	# [0,0,1],
+	# [0,1,0],
 	[0,1,1],
-	[0,1,0],
-	[0,1,0],
+	[1,0,0],
+	# [1,0,1],
+	# [1,1,0],
+	# [1,1,1],
 ]
-print(data);
-bits=3
-
 data2=[]
-for x in range(len(data)-1):
-	for y in range(x+1, len(data)):
+bits=3
+count=len(data)
+countChild=len(data[0])
+isSingle=False
+
+if len(data) == 1:
+	isSingle=True
+if isSingle:
+	count=1
+
+for x in range(1, count+1):
+	begin=x+1
+	if isSingle:
+		begin=begin-1
+	print(x, begin)
+	for y in range(begin, count):
 		r=[]
-		for z in range(len(data[0])):
-			# print(x,y,z,z)
+		print(countChild)
+		for z in range(countChild):
+			print(x,y,z,z)
 			r.append(-1)
-			# print(data[x][z], data[y][z])
+			print(data[x][z], data[y][z])
 			if data[x][z] == data[y][z]:
 				r[z]=data[x][z]
 		data2.append(r)
@@ -35,7 +50,6 @@ for x in range(len(data2)):
 			g.append(alphas[y] + "'")
 		elif data2[x][y] == 1:
 			g.append(alphas[y])
-		#print(x,y)
 	if len(g) > 1:
 		formula.append(g)
 
@@ -59,29 +73,12 @@ for x in range(len(formula)):
 	else:
 		line_edges.append(formula[x][:-1])
 
-# for x in range(len(formula)):
-# 	formula[x].append(1)
-# 	for y in range(bits):
-# 		if formula[x][y][ len(formula[x][y]) -1] == "'":
-# 			formula[x][ len(formula[x]) -1]=0
-# 			break
-# 	if formula[x][ len(formula[x]) -1] == 0:
-# 		dashed_edges.append(formula[x][:-1])
-# 	else:
-# 		line_edges.append(formula[x][:-1])
-
-
 print(formula)
-
-# print("")
-# print(line_edges)
-# print(dashed_edges)
 
 if len(line_edges) != 0:
 	line_edges.append([line_edges[len(line_edges)-1][ len(line_edges[len(line_edges)-1]) -1], "1"])
 if len(dashed_edges) != 0:
 	dashed_edges.append([dashed_edges[len(dashed_edges)-1][ len(dashed_edges[len(dashed_edges)-1]) -1], "1"])
-# dashed_edges.append()
 
-# print(line_edges)
-# print(dashed_edges)
+print(line_edges)
+print(dashed_edges)
