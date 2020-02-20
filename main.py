40bc53e4e4b00e93ebb6187b1227d091f651eadb
Max Base <Max@Asrez.com>
2020-02-21 03:21:18 +0330

    isOcta()

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ from builtins import range
 # from tkinter import ttk
 # import matplotlib.pyplot as plt
 # import networkx as nx
+
+def isOcta(number):
+	isValid=True
+	base=8
+	if number == "":
+		isValid=False
+	for char in number:
+		if char.isdigit() and int(char)<base: 
+			continue
+		else:
+			isValid=False
+	return isValid
